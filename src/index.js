fb41e7cc0e5844d549c42d5042a359ec3e57f6b8
github username <joshmrallen@gmail.com>
2020-08-25 14:02:26 -0400

    implemented new task event listener

--- src/index.js
+++ src/index.js
@@ -1,3 +1,52 @@
+// 1. save the input
+// 2. create li with the user input
+// 3. select #tasks
+// 4. append li
+
+
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  
+  
+  let field = document.getElementById("new-task-description");
+
+  function taskName() {
+    let t = field.value;
+    return t;
+  }
+
+  let submit = document.querySelector("#create-task-form").lastElementChild
+  let taskList = document.querySelector("#tasks");
+
+  submit.addEventListener("click", function(e){
+    e.preventDefault();
+    let li = document.createElement("li")
+    li.innerText = taskName();
+    taskList.appendChild(li);
+    field.value = "";
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
