8b84ad01d2675ab252baa86ed5a9aa1f453bc268
David-Kirsch <davidjkirsch@gmail.com>
2020-08-25 15:07:17 -0400

    Added event listener to submit button

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const taskLister = document.getElementById('list')
 });
+const list = document.getElementById('tasks')
+const field = document.getElementById('new-task-description')
+const submission = document.getElementById('create-task-form')
+
+submission.addEventListener('submit', function(e){
+  e.preventDefault();
+  let newField = document.createElement('li')
+  let content = document.createTextNode(e.target.children[1].value)
+  newField.appendChild(content);
+  list.appendChild(newField);
+});
+
+ 
+const button = document.getElementById("remove")
+button.addEventListener('click', function() {
+  let task  = document.getElementById('tasks'); 
+  // task.innerHTML = "Click on button to remove the element."; 
+    
+  function remove(e) {
+    e.target.getElementById('tasks').remove;
+  }
+})
+
