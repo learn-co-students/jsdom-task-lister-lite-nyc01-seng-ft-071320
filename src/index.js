4d3ff1f4759c5fe37e6de64a7c2431c0af1c7c5b
amitdesh <amitdesh2014@gmail.com>
2020-08-29 18:58:14 -0400

    refactored clean code

--- src/index.js
+++ src/index.js
@@ -1,65 +1,89 @@
-// 1. save the input
-// 2. create li with the user input
-// 3. select #tasks
-// 4. append li
 
-
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-  
-  
-  let field = document.getElementById("new-task-description");
-
-  function taskName() {
-    let t = field.value;
-    return t;
-  }
-
-  let submit = document.querySelector("#create-task-form").lastElementChild
-  let taskList = document.querySelector("#tasks");
-
-  submit.addEventListener("click", function(e){
-    e.preventDefault();
-    let li = document.createElement("li")
-    let del = document.createElement("button")
-    del.type = "button";
-    del.innerText = "X";
-    let selection = document.getElementById("selection");
-    let textColor = selection.value;
-    //if selection value equal to color, 
-    li.style.color = textColor
+document.addEventListener("DOMContentLoaded", function(e){
     
-    del.addEventListener("click", function(e) {
-      li.remove()
-    });
-    li.innerText = taskName();
-    li.appendChild(del);
-    taskList.appendChild(li);
-    field.value = "";
-  });
+  createTask()
+  
+  
+  
+})
 
-  let sortButton = document.querySelector('#sort');
+const submitButton = document.querySelector("#create-task-form").lastElementChild
+let taskList = document.querySelector("#tasks")
+let taskDescriptionField = document.querySelector("#new-task-description")
+const redDiv = document.createElement("div")
+redDiv.id = "red"
+taskList.append(redDiv)
+const yellowDiv = document.createElement("div")
+yellowDiv.id = "yellow"
+taskList.append(yellowDiv)
+const greenDiv = document.createElement("div")
+greenDiv.id = "green"
+taskList.append(greenDiv)
 
-  sortButton.addEventListener("click", function(e){
-    let currentTasks = document.querySelectorAll("li");
-    let redDiv = document.querySelector("#redDiv");
-    let yellowDiv = document.querySelector("#yellowDiv");
-    let greenDiv = document.querySelector("#greenDiv");
 
-    for(let i=0; i<currentTasks.length; i++) {
-      let li = currentTasks[i];
-      if (li.style.color === "red") {
-        redDiv.appendChild(li);
-      } else if (li.style.color === "yellow") {
-        yellowDiv.appendChild(li);
-      } else if (li.style.color === "green") {
-        greenDiv.appendChild(li);
-      }
-    }
-  });
+
+function createTask (){
+  
+  submitButton.addEventListener("click", function(e){
+      e.preventDefault()
+      taskInfo = document.querySelector("#new-task-description").value
+      let task = document.createElement("li")
+      task.innerText = taskInfo
+      task.className = "task"
+      deleteButton(task)
+      addPriorityToTask(task)
+      sortTask(task)
+      // editTask(task)
+      
+  })
+}
+
+function deleteButton(taskBullet){
+  
+  let deleteButton = document.createElement("button")
+  deleteButton.type = "button"
+  deleteButton.innerText = "X"
+  taskBullet.appendChild(deleteButton)
+  
+  deleteButton.addEventListener("click", function(e){
+      e.target.parentNode.remove()
+      
+  })
+}
+
+function addPriorityToTask(task){
+  
+  let priorityDropDown = document.querySelector("#selection")
+  let fontColor = priorityDropDown.value
+  task.style.color = fontColor
+  task.dataset.colorvalue = fontColor
+  
+}
+
 
   
 
-  });
+function sortTask(task){
 
-  
+  if (task.dataset.colorvalue === "red"){
+      redDiv.appendChild(task)
+      editTask(task)
+  }
+  if (task.dataset.colorvalue === "yellow"){
+      yellowDiv.appendChild(task)
+      editTask(task)
+  }
+  if (task.dataset.colorvalue === "green"){
+      greenDiv.appendChild(task)
+  }
+}
+
+
+// function editTask(task){
+//     let editButton = document.createElement("button")
+//     editButton.type = "button"
+//     editButton.innerText = "Edit Task"
+//     task.append(editButton)
+
+
+// }
